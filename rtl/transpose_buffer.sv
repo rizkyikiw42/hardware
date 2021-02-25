cf21bc1e23df1436d8483b1cb1b8b1a3eb99e765
rizkyikiw42 <rizzikwann@gmail.com>
2021-02-25 15:21:12 -0800

    tested TRB. Changed transpose_buffer for timing correction

--- rtl/transpose_buffer.sv
+++ rtl/transpose_buffer.sv
@@ -12,7 +12,7 @@ module transpose_buffer
 
   buf_mem buff (clk, S_in, S_out, raddr, waddr, wren);
 
-  enum {START, NEXT, SWITCH} state;
+  enum {START, NEXT} state;
 
   always_ff @(posedge clk) begin
     if (rst) begin
@@ -20,30 +20,28 @@ module transpose_buffer
       wren <= 0;
     end
 
-    if (ena_in) begin
-      wren <= 1;
+    else begin
+      if (ena_in) begin
+        wren <= 1;
 
-      case (state)
-        START: begin
-          wbuf <= 0;
-          {xw, yw, xr, yr} <= 0;
-          state <= NEXT;
-        end // START
-        NEXT: begin
-          if ({yw, xw} == 6'b111111 && {xr, yr} == 6'b111111) state <= SWITCH;
-          if (xw <= 3'b111) yw <= yw + 1;
-          if (yr <= 3'b111) xr <= xr + 1;
-          xw <= xw + 1;
-          yr <= yr + 1;
-        end // NEXT
-        SWITCH: begin
-          wbuf <= ~wbuf;
-          state <= NEXT;
-        end // SWITCH
-      endcase
-    end // if (ena_in)
+        case (state)
+          START: begin
+            wbuf <= 0;
+            {xw, yw, xr, yr} <= 0;
+            state <= NEXT;
+          end // START
+          NEXT: begin
+            if ({yw, xw} == 6'b111111 && {xr, yr} == 6'b111111) wbuf <= ~wbuf;
+            if (xw == 3'b111) yw <= yw + 1;
+            if (yr == 3'b111) xr <= xr + 1;
+            xw <= xw + 1;
+            yr <= yr + 1;
+          end // NEXT
+        endcase
+      end // if (ena_in)
 
-    else wren <= 0;
+      else wren <= 0;
+    end // else begin
   end // always_ff
 
 endmodule // transpose_buffer
