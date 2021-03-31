fd9c2f3a5440243dd5b7f9c4864f5a2cea804843
rizkyikiw42 <rizzikwann@gmail.com>
2021-03-30 21:01:56 -0700

    Add u-boot

--- build.sh
+++ build.sh
@@ -13,24 +13,42 @@ cd /src/
 export ARCH=arm
 export CROSS_COMPILE=arm-eabi-
 
-pushd hw/quartus
-make rbf
-make dtb
-make preloader
+# pushd hw/quartus
+# make rbf
+# make dtb
+# make preloader
 
-pushd software/spl_bsp
+# pushd software/spl_bsp
 
-[ ! -d "u-boot-socfpga" ] \
-    && git clone --depth=1 --branch=ACDS20.1STD_REL_GSRD_PR https://github.com/altera-opensource/u-boot-socfpga
+# [ ! -d "u-boot-socfpga" ] \
+#     && git clone --depth=1 --branch=ACDS20.1STD_REL_GSRD_PR https://github.com/altera-opensource/u-boot-socfpga
 
-pushd u-boot-socfpga
+# pushd u-boot-socfpga
 
-./arch/arm/mach-socfpga/qts-filter.sh cyclone5 ../../../ ../ ./board/terasic/de1-soc/qts/
+# ./arch/arm/mach-socfpga/qts-filter.sh cyclone5 ../../../ ../ ./board/terasic/de1-soc/qts/
 
-make socfpga_cyclone5_defconfig
-make -j5
+# make socfpga_cyclone5_defconfig
+# make -j5
+
+# popd
+# popd
+# popd
+
+pushd sw/sdcard
+../../hw/quartus/software/spl_bsp/u-boot-socfpga/tools/mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n "Cyclone V script" -d u-boot.txt u-boot.scr
+popd
+
+pushd sw
+[ ! -d "linux-socfpga" ] \
+    && git clone --depth=1 --branch=ACDS20.1STD_REL_GSRD_PR https://github.com/altera-opensource/linux-socfpga
+
+pushd linux-socfpga
+
+make socfpga_defconfig
+make -j 5 zImage modules
+make modules_install INSTALL_MOD_PATH=modules_install
+rm -rf modules_install/lib/modules/*/build
+rm -rf modules_install/lib/modules/*/source
 
 popd
 popd
-popd
-
