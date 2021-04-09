705c952fe9748a0b1816cb62e142d181d07ec29b
rizkyikiw42 <rizzikwann@gmail.com>
2021-04-08 22:47:58 -0700

    Start client toplevel code

--- sw/de1_client/src/client/client.go
+++ sw/de1_client/src/client/client.go
@@ -1,21 +1,9 @@
 package main
 
-import (
-	"os"
+import "log"
 
-	"github.com/CPEN391-Team-4/backend/sw/de1_client/src/camera"
-)
-
-// Do a test capture
 func main() {
-	camera.Open()
-	defer camera.Close()
+	log.Println("starting up")
 
-	camera.Start()
-	camera.Capture()
-	out, _ := camera.Capture()
-	camera.Stop()
-	f, _ := os.Create("out.jpg")
-	f.Write(out)
-	f.Close()
+	log.Println("shutting down")
 }
